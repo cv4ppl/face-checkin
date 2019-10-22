e4e47d8cb8011610bfbdca6e3ce89f762706e8ab
schureed <schureedgood@outlook.com>
2019-10-22 20:10:45 +0800

    Implement model using PCA.
    To see the performance, simply run
    
    ```
    python src/model/single_face_model.py
    ```
    
    to see the accuracy on testing set.

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ def main():
 
 if __name__ == '__main__':
 	tornado.options.define('port', 8848, int, "port to serve")
-	tornado.options.define('db_absl_path', type=str, help="database to load")
+	tornado.options.define('db_absl_path', 'data/database', type=str, help="database to load")
+	tornado.options.define('data_path', 'data', type=str, help="database to load")
 	tornado.options.parse_command_line()
 	main()
