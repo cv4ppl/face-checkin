0b77c79c16f76db34cb71795319878a64fe26078
Hu-chi <25957211+Hu-chi@users.noreply.github.com>
2019-10-22 14:22:55 +0800

    fix bug in win10

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
+import os
 import tornado.options
 
 from src.server.server import Server
+
+def init():
+	default_tmp_path = "tmp"
+	if not os.path.exists(default_tmp_path):
+		os.makedirs(default_tmp_path)
 
 
 def main():
@@ -8,6 +14,7 @@ def main():
 
 
 if __name__ == '__main__':
+	init()
 	tornado.options.define('port', 8848, int, "port to serve")
 	tornado.options.define('db_absl_path', type=str, help="database to load")
 	tornado.options.parse_command_line()
