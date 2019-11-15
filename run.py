ee5ec7a9319f216e955a84d02c93bbfd8efefe3d
Schureed <schureedgood@outlook.com>
2019-11-15 19:48:34 +0800

    Add debug mode.

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ from tornado.options import define, parse_command_line
 define('port', 8848, int, "port to serve")
 define('db_absl_path', os.path.join('data', 'database'), type=str, help="database to load")
 define('data_path', 'data', type=str, help="database to load")
+define('debug', True, type=bool, help="Debug mode")
+
 parse_command_line()
 
 
